ec08b5043d78483fd4e73684d4060ad76dc1cd4d
attariq88 <tarik88sep@gmail.com>
2021-05-03 19:49:17 -0400

    Initial commit

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.HashMap;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -10,6 +12,30 @@ public class DuplicateWord {
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
+        HashMap<String, Integer> totalOccurences = new HashMap<>();
+        HashMap<String, Integer> multipleOccurences = new HashMap<>();
+
+        String[] arrayOfIndividualWords = st.toUpperCase().replace(".","").split(" ");
+
+        for (String individualWords : arrayOfIndividualWords) {
+            Integer previousCount = totalOccurences.get(individualWords);
+            if (previousCount == null){
+                previousCount = 0;
+            }
+            totalOccurences.put(individualWords,previousCount+1);
+
+            if ((previousCount+1) >1){
+                int totalCount = previousCount +1;
+                multipleOccurences.put(individualWords,totalCount);
+            }
+
+        }
+
+        System.out.println(totalOccurences);
+        System.out.println(multipleOccurences);
+
     }
 
 }
+
+
