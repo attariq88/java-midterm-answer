f3e8fc130b1afa3da6de59c19df8f3eb86b2c3f3
attariq88 <tarik88sep@gmail.com>
2021-05-04 00:31:11 -0400

    Initial commit

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -9,5 +9,20 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        String str = "ABC";
+        computePermutation(str,"");
+    }
+
+    public static void computePermutation(String ques, String asf){
+        if(ques.length() == 0){
+            System.out.println(asf);
+        }
+        for (int i = 0; i < ques.length(); i++) {
+            char ch = ques.charAt(i);
+            String qlpart = ques.substring(0,i);
+            String qrpart = ques.substring(i+1);
+            String roq = qlpart + qrpart;
+            computePermutation(roq, asf+ch);
+        }
     }
 }
