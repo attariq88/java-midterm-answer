77bb6ff9fd0a0644bdac6dbb874bd37fabd72d29
attariq88 <tarik88sep@gmail.com>
2021-05-05 05:12:40 -0400

    Final commit

--- src/design/EmployeeAbstract.java
+++ src/design/EmployeeAbstract.java
@@ -1,2 +1,50 @@
-package design;public class EmployeeAbstract {
+package design;
+
+import java.util.Date;
+
+public abstract class EmployeeAbstract implements Employee {
+    @Override
+    public int employeeId() {
+        return 0;
+    }
+
+    @Override
+    public String employeeName() {
+        return null;
+    }
+
+    @Override
+    public String address() {
+        return null;
+    }
+
+    @Override
+    public String email() {
+        return null;
+    }
+
+    @Override
+    public Date dateOfBirth() {
+        return null;
+    }
+
+    @Override
+    public void assignDepartment() {
+
+    }
+
+    @Override
+    public String employeePosition() {
+        return null;
+    }
+
+    @Override
+    public int calculateSalary() {
+        return 0;
+    }
+
+    @Override
+    public void benefitLayout() {
+
+    }
 }
