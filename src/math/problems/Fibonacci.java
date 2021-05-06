e83942f137f165685cb52adc82f8ebe892606eb2
attariq88 <tarik88sep@gmail.com>
2021-05-05 22:27:01 -0400

    Final commit

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -14,7 +14,7 @@ public class Fibonacci {
         int count = 40;
 
         for (int i = 0; i < count; i++) {
-            System.out.print(number1+" ");
+            System.out.print(number1 + " ");
             sum = number1 + number2;
             number1 = number2;
             number2 = sum;
